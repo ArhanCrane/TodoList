34d877406eeef565813f641d1a4bfc8ec8fbc624
ArhanCrane <123109947+ArhanCrane@users.noreply.github.com>
2025-08-30 17:18:43 +0300

    Update db_1.sql

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -3,6 +3,15 @@ DROP SCHEMA IF EXISTS hello_schema CASCADE;
 CREATE SCHEMA IF NOT EXISTS hello_schema;
 
 CREATE TABLE IF NOT EXISTS hello_schema.users (
-    name TEXT PRIMARY KEY,
-    count INTEGER DEFAULT(1)
+    name_ VARCHAR(32) PRIMARY KEY,
+    password_ VARCHAR(64)
+);
+
+
+CREATE TABLE IF NOT EXISTS hello_schema.items (    
+    name_ VARCHAR(32),
+    title_ VARCHAR(64) PRIMARY KEY,
+    description_ VARCHAR(64),
+
+    FOREIGN KEY (name_) REFERENCES hello_schema.users(name_)
 );
